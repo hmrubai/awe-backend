1a2983843c9bd156166efed01506a17a8dc777a1
hmrubai <rubai.mobarak@gmail.com>
2022-12-13 17:23:34 +0600

    Model Created

---
+++ app/Http/Controllers/MasterSettingsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Models\PackageType;
+use Illuminate\Http\Request;
+
+class MasterSettingsController extends Controller
+{
+    
+}
