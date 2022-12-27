faafde7ed4bffe3a2208faa180f5bdff6f9f7d18
hmrubai <rubai.mobarak@gmail.com>
2022-12-27 12:01:56 +0600

    School Schema Added

--- app/Models/SchoolInformation.php
+++ app/Models/SchoolInformation.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class SchoolInformation-m extends Model
+class SchoolInformation extends Model
 {
     use HasFactory;
 }
